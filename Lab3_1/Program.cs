f032d18c9b28e89b48209a90d4afa0f113e14d9e
Ryan Coughran <ryancoughran@quickenloans.com>
2021-07-26 12:41:31 -0700

    Initial commit

---
+++ Lab3_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lab3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lab3.1: Studetn Information");
+            Console.WriteLine("Hello from lab 3.1");
+        }
+    }
+}
